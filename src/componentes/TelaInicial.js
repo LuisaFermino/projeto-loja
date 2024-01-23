deb3a9edf07e52f4d1d9e8907096a201b68d1f55
LuisaFermino <souzafermino49@gmail.com>
2024-01-23 14:54:35 -0300

    Implementação do layout da Tela Inicial

---
+++ src/componentes/TelaInicial.js
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+import Button from "@mui/material/Button";
+
+import Logo from "../assets/img/logo.png";
+
+function TelaInicial({ visivel, setVisivel }) {
+  return visivel ? (
+    <Container>
+      <Main>
+        <LogoLoja>
+          <img src={Logo} alt="Logo" />
+          <p>Nectar</p>
+        </LogoLoja>
+
+        <Titulo>Bem vindo a nossa Loja</Titulo>
+        <Subtitulo>Faça suas compras em apenas uma hora</Subtitulo>
+
+        <Footer>
+          <Button
+            variant="contained"
+            color="success"
+            onClick={() => setVisivel(false)}
+          >
+            Entrar
+          </Button>
+        </Footer>
+      </Main>
+    </Container>
+  ) : (
+    <></>
+  );
+}
+
+const Container = styled.div`
+  background-color: #d7dbdd;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+const Main = styled.div`
+  width: 100vw;
+`;
+
+const LogoLoja = styled.div`
+  display: flex;
+  justify-content: center;
+
+  p {
+    font-weight: 700;
+    font-family: "Oswald", sans-serif;
+    margin-top: 5vh;
+    font-size: 40px;
+  }
+
+  img {
+    width: 30vw;
+    height: 15vh;
+  }
+`;
+const Titulo = styled.p`
+  font-weight: 700;
+  font-family: "Noto Sans JP", sans-serif;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 10vh;
+`;
+
+const Subtitulo = styled.p`
+  font-weight: 400;
+  font-family: "Noto Sans JP", sans-serif;
+  font-size: 12px;
+  margin-top: 2vh;
+  text-align: center;
+`;
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 4vh;
+`;
+
+export default TelaInicial;
