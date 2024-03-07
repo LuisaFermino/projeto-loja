04845d6b1b006be8cf96aaf9835f55446d1e54ca
LuisaFermino <souzafermino49@gmail.com>
2024-03-07 15:28:09 -0300

    Implementação de calculo total para carrinho

--- src/componentes/EnviaMensagem.js
+++ src/componentes/EnviaMensagem.js
@@ -1,21 +1,31 @@
 function EnviaMensagem(cart) {
+  let valorTotal = 0;
   let mensagem = `
   *Itens do Carrinho*
  
-    ---------------------------
- 
+  ---------------------------
+  
  `;
+
   cart.forEach((item) => {
     mensagem += `
-    Produto: ${item.title} 
-    Preço: ${item.price}
+  *Produto:* ${item.title} 
+
+  *Preço: R$ ${item.price}* 
     
-    ---------------------------
+  ---------------------------
+ 
     `;
+    valorTotal += item.price;
   });
 
+  mensagem += `
+  *Total: ${valorTotal}*
+    
+    `;
+
   mensagem = encodeURI(mensagem);
-  window.location.href = `https://wa.me/5514996047024?text=${mensagem}`;
+  window.location.href = `https://wa.me/5514998812492?text=${mensagem}`;
 }
 
 export default EnviaMensagem;
